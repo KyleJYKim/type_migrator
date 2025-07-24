9367f39d38a6f2ca0437864583e930905a982b3f
KyleJYKim <kimjuyoung656@icloud.com>
2025-07-24 15:42:30 +0200

    Initiating Type_Migrator

---
+++ lib/type_migrator.ex
@@ -0,0 +1,18 @@
+defmodule TypeMigrator do
+  @moduledoc """
+  Documentation for `TypeMigrator`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TypeMigrator.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/type_migrator_test.exs
@@ -0,0 +1,8 @@
+defmodule TypeMigratorTest do
+  use ExUnit.Case
+  doctest TypeMigrator
+
+  test "greets the world" do
+    assert TypeMigrator.hello() == :world
+  end
+end
