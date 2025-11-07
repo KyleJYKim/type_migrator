ec703654c165e9a6abdaddf56e35334f6ec2d1cb
KyleJYKim <kimjuyoung656@icloud.com>
2025-11-07 19:48:48 +0100

    document updated

--- lib/ex1.ex
+++ lib/ex1.ex
@@ -5,18 +5,19 @@ defmodule Ex1 do
   @spec strong_identity(integer()) :: integer()
   def strong_identity(x) when is_integer(x), do: x
 
-  @spec inc({<<_::_*8>>, %{required(:one) => integer(), binary() => integer()}}) :: integer()
-  def inc({:a, tail}), do: (tail |> Map.get(:one)) + 1
+  # @spec inc({<<_::_*8>>, %{required(:one) => integer(), binary() => integer()}}) :: integer()
+  # def inc({:a, tail}), do: (tail |> Map.get(:one)) + 1
 
-  @spec id({any()}) :: {any()}
+  @spec id(:a) :: any()
   def id(ts), do: ts
 
   defmodule In do
-    @spec inc() :: integer()
-    def inc(), do: Ex1.inc({:"#{<<97::1*8>>}", %{:one => 1, "two" => 2, 3 => 3, 4 => 4}})
+    # @spec inc() :: integer()
+    # def inc(), do: Ex1.inc({:"#{<<97::1*8>>}", %{:one => 1, "two" => 2, 3 => 3, 4 => 4}})
 
-    @spec id([a: integer()]) :: {b} when a: b, b: a
+    @spec id(list(list())) :: any()
     def id(ts), do: Ex1.id(ts)
+
   end
 
   defmodule Some do
