6aa05724285d038880c7513098a486097458aaf7
KyleJYKim <kimjuyoung656@icloud.com>
2026-01-30 00:09:38 +0100

    delete top function approx and fix field approximation

--- lib/migrator/translator.ex
+++ lib/migrator/translator.ex
@@ -271,27 +271,29 @@ defmodule Migrator.Translator do
           end)
         end
         case {type1, type2} do
-          {{{:union, left1, right1}, {:union, left2, right2}}, _} ->
+          {{:union, left1, right1}, {:union, left2, right2}} ->
             [{left1, left2} |> comparison_fun.(), {left1, right2} |> comparison_fun.(), {right1, left2} |> comparison_fun.(), {right1, right2} |> comparison_fun.()]
               |> match_accumulator.()
-          {{{:union, left1, right1}, _}, _} ->
+          {{:union, left1, right1}, _} ->
             [{left1, type2} |> comparison_fun.(), {right1, type2} |> comparison_fun.()]
               |> match_accumulator.()
-          {{_, {:union, left2, right2}}, _} ->
+          {_, {:union, left2, right2}} ->
             [{left2, type1} |> comparison_fun.(), {right2, type1} |> comparison_fun.()]
               |> match_accumulator.()
           {:atom, {:atom, _}} -> {:partial_match, [type2]}
           {{:atom, _}, :atom} -> {:partial_match, [type1]}
+          {{:supertyped, type1}, {:supertyped, type1}} -> if type1 == type2, do: {:partial_match, [type2]}, else: {:no_match, []}
+          {_, {:supertyped, type1}} -> if type1 == type2, do: {:partial_match, [type2]}, else: {:no_match, []}
           {{:supertyped, type1}, _} -> if type1 == type2, do: {:partial_match, [type2]}, else: {:no_match, []}
           _ -> if type1 == type2, do: {:all_match, [type2]}, else: {:no_match, []}
         end
       end
 
     approximated_merging = fn {{{key_type_l1, val_type_l1}, {key_type, val_type}}, match_list} ->
-
+        key_type = key_type |> get_lower_bound_super_key_type()
         case key_type do
-          {:supertyped, key_type} -> :not_yet
-
+          {:union, left, right} -> :not_yet
+          {:supertyped, key_type} -> match_list |> Enum.reduce([], fn type -> end)
           _ -> :not_yet
         end
 
@@ -349,8 +351,6 @@ defmodule Migrator.Translator do
         if field_new == :empty, do: L1, else: [field_new | L1]  # natural reverse.. but hmm..
       end
 
-
-    # MAKE A CASE WHEN L1 IS EMPTY!!!
     fields |> Enum.reduce([{:empty, :empty}], fn field, L1 -> field |> total_merging.(L1) end)
   end
 
