6a3a620362826b8f74dfc175f0de0b76d70f7f36
KyleJYKim <kimjuyoung656@icloud.com>
2026-03-04 11:41:42 +0100

    fix type extractor_type function

--- lib/migrator/type_translator.ex
+++ lib/migrator/type_translator.ex
@@ -4,16 +4,23 @@ defmodule Migrator.TypeTranslator do
 
   def process(paths) do
     extracted_types = paths |> Enum.map(fn path ->
-      File.read!
+      path |> File.read!
       |> Code.string_to_quoted!
-      |> extract_type           |> IO.inspect(label: "### EXTRACT TYPE FUNCTION RESULT \n")
+      |> extract_type
+
+      # |> Enum.reduce(%{}, fn {x, y}, acc -> # for debugging
+      #   %{x => y} |> IO.inspect(label: "### EXTRACT TYPE FUNCTION RESULT \n")
+      #   acc |> Map.put(x, y)
+      # end)
     end)
+    |> Enum.reduce(%{}, fn elem, acc -> Map.merge(acc, elem |> IO.inspect(label: "### EXTRACT TYPE FUNCTION RESULT \n")) end)
+
     # IO.puts("\nQUOTED \n")
     # quoted |> IO.inspect()
 
     translated_types = extracted_types
-      |> parse_type()           |> Enum.map(fn x -> x |> IO.inspect(label: "\n ### PARSE TYPE FUNCTION RESULT \n") end)
-      #|> translate_type()       |> Enum.map(fn x -> x |> IO.inspect(label: "\n ### TRANSLATE TYPE FUNCTION RESULT \n") end)
+      |> parse_type()           |> Enum.reduce(%{}, fn {k, v}, acc -> Map.merge(acc, %{k => v} |> IO.inspect(label: "\n ### PARSE TYPE FUNCTION RESULT \n")) end)
+      #|> translate_type()       |> Enum.reduce(%{}, fn {k, v}, acc -> Map.merge(acc, %{k => v} |>  IO.inspect(label: "\n ### TRANSLATE TYPE FUNCTION RESULT \n")) end)
 
     translated_types
   end
@@ -22,7 +29,7 @@ defmodule Migrator.TypeTranslator do
 
     extractor = fn {ast, module, acc}, extractor_fun ->
       extractor_fun = &extractor_fun.(&1, extractor_fun)
-      case ast |> IO.inspect(label: AST) do
+      case ast do
         {:defmodule, _, [{:__aliases__, _, [module_name]}, [do: module_ast]]} ->
           module = if module == "", do: "#{module_name}", else: "#{module}.#{module_name}"
           {module_ast, module, acc} |> extractor_fun.()
@@ -40,11 +47,13 @@ defmodule Migrator.TypeTranslator do
             end)
           new_acc
 
+          # also the one with parentheses
+
         _ -> acc
       end
     end
 
-    {ast, "", []} |> extractor.(extractor)
+    {ast, "", %{}} |> extractor.(extractor)
   end
 
   defp parse_type(extracted_types) do
@@ -52,28 +61,28 @@ defmodule Migrator.TypeTranslator do
     total_parser = fn {module, type_defs} -> (
 
       type_defs = type_defs |> Enum.map(fn {user_defined_type, defining_type} ->
-          {user_defined_type, defining_type |> parse}
+          {user_defined_type |> parse, defining_type |> parse}
         end)
 
-      {module, type_defs}
+      %{module => type_defs}
     )end
 
-    extracted_types |> Enum.map(fn type_definition -> type_definition |> Map.to_list |> Enum.map(total_parser) end)
+    extracted_types |> Enum.reduce(%{}, fn type_definition, acc -> acc |> Map.merge(type_definition |> total_parser.()) end)
   end
 
-  defp translate_spec(parsed_type_tree) do
+  # defp translate_spec(parsed_type_tree) do
 
-    total_translator = fn {line_num, name, inputs, output, guards} -> (
+  #   total_translator = fn {line_num, name, inputs, output, guards} -> (
 
-      translation = &translate(&1, guards)
-      inputs = inputs |> Enum.map(fn input -> input |> translation.() end)
-      output = output |> translation.()
-      guards = if guards == nil, do: nil, else: guards |> Enum.map(fn {var, type} -> {var, type |> translation.()} end)
+  #     # need to replace the defined types to the org types. Look in the list!
+  #     # AFTER TRANSLATION
+  #     #type_defs = type_defs |>
+  #     inputs = inputs |> Enum.map(fn input -> input |> translation.() end)
 
-      {line_num, name, inputs, output, guards}
-    )end
+  #     {line_num, name, inputs, output, guards}
+  #   )end
 
-    parsed_spec_tree |> Enum.map(total_translator)
-  end
+  #   parsed_spec_tree |> Enum.map(total_translator)
+  # end
 
 end
